3dd9de910ef55da96e0a91d9f2721330133c4093
poddui <jimi8888.podduikin@gmail.com>
2023-10-06 21:46:13 +0300

    Completed

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  const[contact, setContact] = useState([]);
+
+  const getAll = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if( status === 'granted'){
+      const {data} = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers] }
+        );
+      if(data.length > 0 ) {
+        setContact(data);
+      }
+    }
+  }
+
+  const listSeparator = () => {
+    return(
+      <View
+        style={{
+          height: 1,
+          width: "100%", 
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <FlatList
+            style={styles.wholeList}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({item}) => {
+              return(
+                <View style={styles.list}>
+                  <Text style={styles.listText}>{item.name}, {item.phoneNumbers[0]?.number}</Text>
+                </View>
+              );
+            }}
+            ItemSeparatorComponent={listSeparator}
+            data={contact}
+          />
+      <Button
+        title="Get Contacts"
+        onPress={getAll}>
+      </Button>
     </View>
   );
 }
@@ -15,6 +57,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  list: {
+    marginTop: 5,
+    padding: 30,
+  },
+  listText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  infoText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 400,
+  },    
+  wholeList: {
+    marginTop: 25
   },
 });
+
